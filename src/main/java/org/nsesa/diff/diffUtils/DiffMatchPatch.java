9ce08bf277961eb417635ef1051fdc3ffef6f743
Philip Luppens <philip.luppens@gmail.com>
2013-02-11 15:05:29 +0100

    Issue #1: added copyright and licence header.

--- src/main/java/org/nsesa/diff/diffUtils/DiffMatchPatch.java
+++ src/main/java/org/nsesa/diff/diffUtils/DiffMatchPatch.java
@@ -1,22 +1,16 @@
-/*
- * Diff Match and Patch
+/**
+ * Copyright 2013 European Parliament
  *
- * Copyright 2006 Google Inc.
- * http://code.google.com/p/google-diff-match-patch/
+ * Licensed under the EUPL, Version 1.1 or – as soon they will be approved by the European Commission - subsequent versions of the EUPL (the "Licence");
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * http://joinup.ec.europa.eu/software/page/eupl
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * Unless required by applicable law or agreed to in writing, software distributed under the Licence is distributed on an "AS IS" basis,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * See the Licence for the specific language governing permissions and limitations under the Licence.
  */
-
 package org.nsesa.diff.diffUtils;
 
 import java.io.UnsupportedEncodingException;
@@ -1169,7 +1163,7 @@ class DiffMatchPatch {
         }
 
 		/*
-		 * Second pass: look for single edits surrounded on both sides by
+         * Second pass: look for single edits surrounded on both sides by
 		 * equalities which can be shifted sideways to eliminate an equality.
 		 * e.g: A<ins>BA</ins>C -> <ins>AB</ins>AC
 		 */
--- src/test/java/org/nsesa/diff/diffUtils/AllInOneComplexDiffTest.java
+++ src/test/java/org/nsesa/diff/diffUtils/AllInOneComplexDiffTest.java
@@ -1,3 +1,16 @@
+/**
+ * Copyright 2013 European Parliament
+ *
+ * Licensed under the EUPL, Version 1.1 or – as soon they will be approved by the European Commission - subsequent versions of the EUPL (the "Licence");
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ *
+ * http://joinup.ec.europa.eu/software/page/eupl
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and limitations under the Licence.
+ */
 package org.nsesa.diff.diffUtils;
 
 import org.junit.Assert;
@@ -252,7 +265,7 @@ public class AllInOneComplexDiffTest {
 
     @Test
     public void testSimpleDstMultiwordChangeWithOriginalChange() {
-		/*String original = "This is a test for discovering if changes work";
+        /*String original = "This is a test for discovering if changes work";
 		String modified = "This was something for discovering if changes work";
 		String verified = "This was something fors another discovering if changes work";
 		
